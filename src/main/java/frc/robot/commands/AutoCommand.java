8cdf3d34cad8283668e56b5208a66e2b46063100
Ezelbyr777 <ezelbayar777@gmail.com>
2025-02-09 00:38:01 +0300

    Update AutoCommand.java

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -1,5 +1,37 @@
-package frc.robot.subsystems;
+package frc.robot.commands;
 
-public class AutoCommand {
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.RobotDrive;
 
+public class AutoCommand extends Command {
+  RobotDrive robotDrive;
+  private Timer timer;
+  private double seconds = 1.0;
+
+  public AutoCommand(RobotDrive robotDrive) {
+    this.robotDrive = robotDrive;
+    timer = new Timer();
+    addRequirements(robotDrive);
+  }
+
+  @Override
+  public void initialize() {
+    timer.restart();
+  }
+
+  @Override
+  public void execute() {
+    robotDrive.driveTank(0.5, 0.0);
+  }
+
+  @Override
+  public void end(boolean isInterrupted) {
+    robotDrive.driveTank(0.0, 0.0);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return timer.get() >= seconds;
+  }
 }
