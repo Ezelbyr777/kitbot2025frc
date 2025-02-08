9fc4c93e4326abdbd45c93c66ab9a9cbaae46705
Ezelbyr777 <ezelbayar777@gmail.com>
2025-02-09 00:46:44 +0300

    Update DriveCommand.java

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,5 +1,37 @@
 package frc.robot.commands;
 
-public class DriveCommand {
+import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.RobotDrive;
+
+public class DriveCommand extends Command {
+    
+    private final RobotDrive robotDrive;
+    private final DoubleSupplier forward;
+    private final DoubleSupplier rotation;
+
+    public DriveCommand(DoubleSupplier forward, DoubleSupplier rotation, RobotDrive robotDrive) {
+        this.forward = forward;
+        this.rotation = rotation;
+        this.robotDrive = robotDrive;
+
+        addRequirements(this.robotDrive);
+    }
+  
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {
+        robotDrive.tankMode(forward.getAsDouble(), rotation.getAsDouble());
+    }
+
+    @Override
+    public void end(boolean isInterrupted) {}
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
